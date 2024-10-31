9fd20bc9128f7ad5e51ea729c46db681a176bb5d
zoeycheng <zoeycheng98@gmail.com>
2024-10-31 13:15:50 +1000

    upload files

--- backend/app.py
+++ backend/app.py
@@ -54,5 +54,5 @@ def save_image():
         return jsonify({'error': 'Failed to save image'}), 500
 
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5001)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
--- backend/model.py
+++ backend/model.py
@@ -1,9 +1,27 @@
 import tensorflow as tf
 import numpy as np
+import boto3
+import os
+
+s3 = boto3.client("s3")
+
+BUCKET_NAME = "zappa-drawtodigit"
+MODEL_KEY = "models/mnist_model.h5"
+
+LOCAL_MODEL_PATH = '/tmp/mnist_model.h5'
+
+# model = tf.keras.models.load_model("mnist_model.h5")
+# model.summary()
+
+def load_model_from_s3():
+    if not os.path.exists(LOCAL_MODEL_PATH):
+        s3.download_file(BUCKET_NAME, MODEL_KEY, LOCAL_MODEL_PATH)
 
 
-model = tf.keras.models.load_model("mnist_model.h5")
-model.summary()
+    model = tf.keras.models.load_model(LOCAL_MODEL_PATH)
+    return model
+
+model = load_model_from_s3()
 
 def predict_digit(image):
     """
